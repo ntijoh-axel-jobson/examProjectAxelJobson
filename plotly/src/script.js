d4597fa6f0c47b08672b2103760cab54e87b67f5
ntijoh-axel-jobson <axel.jobson@elev.ga.ntig.se>
2023-05-15 13:12:57 +0200

    plotly work

--- plotly/src/script.js
+++ plotly/src/script.js
@@ -1,5 +1,24 @@
 fetch('https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station/97400/period/latest-months/data.json')
-.then(response => response.json())
-.then(data => {
-  const temperatures = data.value.map(entry => entry.value);
-  const dates = data.value.map(entry => new Date(entry.date));
+    .then(response => response.json())
+    .then(data => {
+    const temperatures = data.value.map(entry => entry.value);
+    const dates = data.value.map(entry => new Date(entry.date));
+
+    const trace = {
+        x: dates,
+        y: temperatures,
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Data'
+      };
+
+    const layout = {
+        title: 'Data Chart',
+        xaxis: { title: 'Date' },
+        yaxis: { title: 'Temperature'}
+      };
+    
+      const chartData = [trace]  
+      Plotly.newPlot('tester', chartData, layout);
+      
+    })
